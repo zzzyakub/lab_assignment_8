e2355d15ec5e9b1d3f118906a94c4dc97e76b218
zzzyakub <jacobjohnklopp@gmail.com>
2023-07-14 22:03:23 -0400

    Lab Assignment 8

--- sort.c
+++ sort.c
@@ -1,13 +1,38 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 int extraMemoryAllocated;
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+    extraMemoryAllocated = 0;
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        int parent = i;
+        int child = 2 * parent + 1;
+
+        while (child < n) {
+            if (child + 1 < n && arr[child + 1] > arr[child])
+                child++;
+
+            if (arr[child] > arr[parent]) {
+                int temp = arr[parent];
+                arr[parent] = arr[child];
+                arr[child] = temp;
+
+                parent = child;
+                child = 2 * parent + 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    // Calculate the extra memory allocated during sorting
+    extraMemoryAllocated = sizeof(int) * n;
 }
 
 
@@ -15,6 +40,55 @@ void heapSort(int arr[], int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+    extraMemoryAllocated = 0;
+
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[n1], R[n2];
+
+        for (i = 0; i < n1; i++)
+            L[i] = pData[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = pData[m + 1 + j];
+
+        i = 0;
+        j = 0;
+        k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                pData[k] = L[i];
+                i++;
+            } else {
+                pData[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            pData[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            pData[k] = R[j];
+            j++;
+            k++;
+        }
+
+        // Calculate the extra memory allocated during sorting
+        extraMemoryAllocated = sizeof(int) * (n1 + n2);
+    }
 }
 
 // parses input file to an integer array
